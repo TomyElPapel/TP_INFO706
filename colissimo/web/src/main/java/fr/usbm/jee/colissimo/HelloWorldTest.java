6e3f2083d51962282c61b3055f50cbe6ea9a396b
TomyElPapel <thomasburfinp@gmail.com>
2024-10-10 17:03:17 +0200

    aaaaaaaaaah

---
+++ colissimo/web/src/main/java/fr/usbm/jee/colissimo/HelloWorldTest.java
@@ -0,0 +1,21 @@
+package fr.usbm.jee.colissimo;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/HelloWorld")
+public class HelloWorldTest extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter writer = resp.getWriter();
+        writer.println("Ratio");
+    }
+}
